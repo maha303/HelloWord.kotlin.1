9973f56f384a9c2dcbf24137bdc982cd1321d439
maha303 <86849690+maha303@users.noreply.github.com>
2021-09-22 14:41:43 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/hellowordkotlin1/MainActivity.kt
+++ app/src/main/java/com/example/hellowordkotlin1/MainActivity.kt
@@ -2,35 +2,43 @@ package com.example.hellowordkotlin1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import kotlin.math.log
 import android.os.Bundle as Bundle1
-import kotlin.io.println as ioPrintln
+import kotlin.io.println
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle1?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         var num1= 10
-        var op = listOf<String>("+","-","*","/")
         var num2= 5
+        //var op = listOf<String>("+","-","*","/")
+        val editText = findViewById<EditText>(R.id.edittext22)
+        val  userInput = editText.text.toString()
 
-       if(op[0]=="+") {
+
+       if(userInput=="+") {
             Log.d("MainActivity", " $num1 + $num2 = ${num1+num2}")
+
         }
-        if(op[1]=="-") {
+        if(userInput=="-") {
            Log.d("MainActivity", " $num1 - $num2 = ${num1-num2}")
        }
-        if(op[2]=="*") {
+        if(userInput=="*") {
            Log.d("MainActivity", " $num1 * $num2 = ${num1*num2}")
        }
-       if(op[3]=="/")  {
-           Log.d("MainActivity", " $num1 / $num2 = ${num1/num2}")
+       if(userInput=="/") {
+           Log.d("MainActivity", " $num1 / $num2 = ${num1 / num2}")
        }
+
         fun remainder (): Int {
             return num1%num2
         }
         Log.d("MainActivity", "onCreate() called with: remainder is  = ${remainder()}")
-    }
-}
+
+}}
+
